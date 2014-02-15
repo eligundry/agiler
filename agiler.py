b702fd2e63832f1a5ce788b37bbc380958191cd5
Eli Gundry <eligundry@gmail.com>
2014-02-15 02:12:21 -0500

    Started adding event and category models Replaced route stubs with restful stubs

--- agiler.py
+++ agiler.py
@@ -1,71 +1,78 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, render_template
+from flask.ext import restful
 import yaml
 
 app = Flask(__name__)
+api = restful.Api(app)
 config = yaml.load(open('config.yml', 'r+'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
-def register():
-    """
-    Create a new user
-    Returns user id and access key
-    POST method
-    """
-    pass
+class Register(restful.Resource):
+    def post(self):
+        """
+        Create a new user
+        Returns user id and access key
+        """
+        pass
 
-@app.route('/login')
-def login():
-    """
-    Login an existing user
-    Returns user id and access key
-    POST method
-    """
-    pass
+api.add_resource(Register, '/register')
 
-@app.route('/events')
-def get_all_events():
-    """
-    Returns all events for a user
-    Requires user_id and event key
-    """
-    pass
+class Login(restful.Resource):
+    def post(self):
+        """
+        Login an existing user
+        Returns user id and access key
+        """
+        pass
 
-@app.route('/event/<int:event_id>')
-def get_single_event(event_id):
-    """
-    """
-    pass
+api.add_resource(Login, '/login')
 
-@app.route('/event/new')
-def new_event():
-    """
-    Create new event for user
-    Requires user_id, access_key, and event details
-    Returns event data
-    """
-    pass
+class Event(restful.Resource):
+    def get(self, event_id):
+        """
+        Gets a single event specified by an event_id
+        Requires user_id and access_key
+        """
+        pass
 
-@app.route('/event/update')
-def update_event():
-    """
-    Updates existing event for user
-    Requires user_id, access_key, and updated event details
-    Returns updated event data
-    """
-    pass
+    def put(self, event_id):
+        """
+        Updates existing event for user
+        Requires user_id, access_key, and updated event details
+        Returns updated event data
+        """
+        pass
 
-@app.route('/event/delete')
-def delete_event():
-    """
-    Deletes existing event for user
-    Required user_id and access_key
-    Returns status code for deleted object
-    """
-    pass
+    def delete(self, event_id):
+        """
+        Deletes existing event for user
+        Required user_id and access_key
+        Returns status code for deleted object
+        """
+        pass
+
+api.add_resource(Event, '/events/<int:event_id>')
+
+class EventList(restful.Resource):
+    def get(self):
+        """
+        Gets all events for the user
+        Requires user_id and access_key
+        """
+        pass
+
+    def post(self):
+        """
+        Create new event for user
+        Requires user_id, access_key, and event details
+        Returns event data
+        """
+        pass
+
+api.add_resource(EventList, '/events')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -5,15 +5,21 @@ from agiler import config
 connection = '%(engine)s://%(user)s:%(pass)s@%(host)s/%(database)s' % config['db']
 
 class User:
-    def __init__(self, user_id=None):
-        self.db = dataset.connect(connection)
-        self.table = self.db['users']
-        self.data = {}
+    self.db = dataset.connect(connection)
+    self.table = self.db['users']
+    self.data = {}
 
+    def __init__(self, user_id=None, email=None):
         if user_id is not None:
             self.data = self.table.find_one(id=user_id)
 
+        if email is not None:
+            self.data = self.table.find_one(email=email)
+
     def create(self, first_name, last_name, email, password):
+        if len(self.data) is not 0:
+            return False
+
         self.data = {
             'first_name': first_name,
             'last_name': last_name,
@@ -23,17 +29,61 @@ class User:
 
         self.table.insert(self.data)
 
-    def login(self, email, password):
-        user = self.table.find_one(email = email)
+    def update(self):
+        pass
 
-        if bcrypt.verify(password, user.data['password']):
-            pass
-        else:
-            return False
+    def login(self, password):
+        return bcrypt.verify(password, self.data['password'])
 
     def get_access_token(self):
         return md5_crypt.encrypt(self.data['password'])
 
     def verify_access_token(self, access_token):
-        return md5_crypt.verify(access_token, mdb5_crypt.encrypt(self.data['password']))
+        return md5_crypt.verify(access_token, md5_crypt.encrypt(self.data['password']))
 
+class Category:
+    self.db = dataset.connect(connection)
+    self.table = self.db['categories']
+    self.data = {}
+
+    def __init__(self, category_id=None, name=None):
+        pass
+
+    def create(self, name, color):
+        self.data = {
+            'name': name,
+            'color': color
+        }
+
+        self.table.insert(self.data)
+
+
+class Event:
+    self.db = dataset.connect(connection)
+    self.table = self.db['events']
+    self.data = {}
+
+    def __init__(self, event_id=None, user_id=None):
+        if event_id is not None:
+            self.data = self.table.find_one(id=event_id)
+
+        if user_id is not None:
+            self.data = self.table.find(user_id=user_id)
+
+    def create(self, name, description, location, recurring, time, duration):
+        if len(self.data) is not 0:
+            return False
+
+        self.data = {
+            'name': name,
+            'description': description,
+            'location': location,
+            'recurring': recurring,
+            'time': time,
+            'duration': duration
+        }
+
+        self.table.insert(self.data)
+
+    def update(self):
+        pass
