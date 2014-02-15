0b84d181ab2a8ab67bd7137dc1b64f6afa49f0d7
Eli Gundry <eligundry@gmail.com>
2014-02-15 00:31:31 -0500

    Got user model with database working

--- models.py
+++ models.py
@@ -1,25 +1,39 @@
 import dataset
+from passlib.hash import bcrypt, md5_crypt
 from agiler import config
 
 connection = '%(engine)s://%(user)s:%(pass)s@%(host)s/%(database)s' % config['db']
 
 class User:
-    def __init__(self):
+    def __init__(self, user_id=None):
         self.db = dataset.connect(connection)
         self.table = self.db['users']
+        self.data = {}
+
+        if user_id is not None:
+            self.data = self.table.find_one(id=user_id)
 
     def create(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
+        self.data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': bcrypt.encrypt(password)
+        }
 
-        self.table.insert(dict(
-            first_name = self.first_name,
-            last_name = self.last_name,
-            email = self.email,
-            password = self.password
-        ))
+        self.table.insert(self.data)
 
-    def login(self, user_id, password):
-        pass
+    def login(self, email, password):
+        user = self.table.find_one(email = email)
+
+        if bcrypt.verify(password, user.data['password']):
+            pass
+        else:
+            return False
+
+    def get_access_token(self):
+        return md5_crypt.encrypt(self.data['password'])
+
+    def verify_access_token(self, access_token):
+        return md5_crypt.verify(access_token, mdb5_crypt.encrypt(self.data['password']))
+
